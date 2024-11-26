6c0cac300198d346a0c6500777f116bb45bef1c0
prasunchakra <lordprasun@gmail.com>
2024-11-26 13:00:07 +0530

    User and Profile Model

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - python manage.py startapp userauths
 - python manage.py startapp api     
 - python manage.py runserver      
+- python manage.py makemigrations   
 - python manage.py migrate
 - python manage.py createsuperuser
     Username (leave blank to use 'prasunchakraborty'): prasun
--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=16)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    otp = models.CharField(max_length=10)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
+    # def save(self, force_insert = ..., force_update = ..., using = ..., update_fields = ...):
+    #     return super().save(force_insert, force_update, using, update_fields)
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="user_folder", default='avatar.jpg',blank=True, null=True)
+    full_name = models.CharField(max_length=100)
+    about = models.TextField(null=True,blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+    
+    # def save(self, force_insert = ..., force_update = ..., using = ..., update_fields = ...):
+    #     return super().save(force_insert, force_update, using, update_fields)
